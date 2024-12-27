8cf75b227f15af4c3471bf9fb668f3d62082afa6
Ivan Antonov <ivan1antonov@gmail.com>
2024-12-27 16:17:54 +0400

    feat: add remaining solutions tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -3,21 +3,54 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  const transformArray = [];
+  let skipNext = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (skipNext) {
+      skipNext = false;
+      continue;
+    }
+    switch (arr[i]) {
+      case '--discard-next':
+        skipNext = true;
+        break;
+      case '--discard-prev':
+        if (transformArray.length > 0 && arr[i - 2] !== '--discard-next') {
+          transformArray.pop();
+        }
+        break;
+      case '--double-next':
+        if (i + 1 < arr.length) {
+          transformArray.push(arr[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if (i > 0 && arr[i - 2] !== '--discard-next') {
+          transformArray.push(arr[i - 1]);
+        }
+        break;
+      default:
+        transformArray.push(arr[i]);
+        break;
+    }
+  }
+  return transformArray;
 }
 
 module.exports = {
-  transform
+  transform,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,33 +3,87 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(trigger = true) {
+    this.trigger = trigger;
+    this.varChart = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(string, key) {
+    if (
+      !string ||
+      !key ||
+      typeof string !== 'string' ||
+      typeof key !== 'string'
+    ) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    string = string.toUpperCase();
+    key = key.toUpperCase();
+
+    let keyIndex = 0;
+    const result = string.split('').map((char) => {
+      if (this.varChart.includes(char)) {
+        const encryptedIndex =
+          (this.varChart.indexOf(char) +
+            this.varChart.indexOf(key[keyIndex % key.length])) %
+          this.varChart.length;
+        keyIndex++;
+        return this.varChart[encryptedIndex];
+      }
+      return char;
+    });
+
+    return this.trigger ? result.join('') : result.reverse().join('');
+  }
+  decrypt(string, key) {
+    if (
+      !string ||
+      !key ||
+      typeof string !== 'string' ||
+      typeof key !== 'string'
+    ) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    string = string.toUpperCase();
+    key = key.toUpperCase();
+
+    let keyIndex = 0;
+    const result = string.split('').map((char) => {
+      if (this.varChart.includes(char)) {
+        const decryptedIndex =
+          (this.varChart.indexOf(char) -
+            this.varChart.indexOf(key[keyIndex % key.length]) +
+            this.varChart.length) %
+          this.varChart.length;
+        keyIndex++;
+        return this.varChart[decryptedIndex];
+      }
+      return char;
+    });
+
+    return this.trigger ? result.join('') : result.reverse().join('');
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
